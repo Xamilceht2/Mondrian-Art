55e6968ddc9cd8545f1d944dfcc940a250284e0a
Xamilceht2 <ethan.cunanan.889@my.aquinas.me>
2025-05-22 13:41:12 +0000

    dfeth

--- main.py
+++ main.py
@@ -2,27 +2,52 @@ from PIL import Image, ImageDraw
 import random as r
 
 def split(tl, br):
-    p1 = (r.randint(tl[0], br[0]), r.randint(br[1], tl[1]))
-    w = r.randint(p1[0], br[0])
-    h = r.randint(br[1], p1[1])
-    p2 = (w, h)
-    draw.line((tl[0], p1[1], p2[0], p1[1]), 0, 2)
-    draw.line((p1[0], p1[1], p1[0], br[1]), 0, 2)
-    draw.line((p1[0], p2[1], br[0], p2[1]), 0, 2)
-    draw.line((p2[0], tl[1], p2[0], p2[1]), 0, 2)
-    a = ((tl[0], tl[1]), (p2[0], p1[1]))
-    b = ((tl[0] , p1[1]), (p1[0], br[1]))
-    c = ((p2[0], tl[1]), (br[0], p2[1]))
-    d = ((p1[0], p2[1]), (br[0], br[1]))
-    e = ((p1[0], p1[1]), (p2[0], p2[1]))
-    if a[0][1] <= 50 or a[1][0] <= 50:
-        return None
-    else:
-        print('running')
-        return split(a[0], a[1])
-    
+    minval = 100
 
-im = Image.new('RGB', (720, 720), (255, 255, 255))
+    if r.randint(0, 10) >9:
+       minval = 500
+
+    if br[0] -tl[0] < minval or br[1] -tl[1] < minval:
+        #draw
+        c = colours[ r.randint(0, len(colours))-1]
+        
+        
+
+        draw.rectangle( [tl,br], fill = c, outline="black", width=2)
+
+
+    else:
+        p1 = (r.randint(tl[0], br[0]), r.randint(tl[1], br[1]))
+        w = r.randint(p1[0], br[0])
+        h = r.randint(p1[1], br[1])
+        p2 = (w, h)
+        '''
+        draw.line((tl[0], p1[1], p2[0], p1[1]), 0, 2)
+        draw.line((p1[0], p1[1], p1[0], br[1]), 0, 2)
+        draw.line((p1[0], p2[1], br[0], p2[1]), 0, 2)
+        draw.line((p2[0], tl[1], p2[0], p2[1]), 0, 2)
+        '''
+        a = ((tl[0], tl[1]), (p2[0], p1[1]))
+        b = ((tl[0], p1[1]), (p1[0], br[1]))
+        c = ((p2[0], tl[1]), (br[0], p2[1]))
+        d = ((p1[0], p2[1]), (br[0], br[1]))
+        e = ((p1[0], p1[1]), (p2[0], p2[1]))
+
+
+        print('running')
+        split(a[0], a[1])
+
+        split(b[0], b[1])
+
+        split(c[0], c[1])
+
+        split(d[0], d[1])
+
+        split(e[0], e[1])
+
+colours = ["#ffff33", "#ffffff", "#00f3ff", "#af00b3"]
+
+im = Image.new('RGBA', (1000, 1000), (0, 0, 0))
 draw = ImageDraw.Draw(im)
-split((0, 720), (720, 0))
+split((0, 0), (1000, 1000))
 im.show()
