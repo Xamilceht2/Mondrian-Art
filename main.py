d698c6b56557ca5e4fbcf4d3ecc43880a939a95e
Xamilceht2 <ethan.cunanan.889@my.aquinas.me>
2025-05-26 13:36:22 +0000

    jnjnjk

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
 from PIL import Image, ImageDraw
 import random as r
 
-def split(tl, br):
-    minval = 100
+class mondo:
+    def __init__(self, tl, br, minval):
+        self.im = Image.new('RGBA', (tl, br), (0, 0, 0))
+        self.minval = minval
+        self.draw = ImageDraw.Draw(self.im)
 
-    if r.randint(0, 10) >9:
-       minval = 500
+        self.colours = ["#ffff33", "#ffffff", "#00f3ff", "#af00b3"]
 
-    if br[0] -tl[0] < minval or br[1] -tl[1] < minval:
-        #draw
-        c = colours[ r.randint(0, len(colours))-1]
-        
-        
+    def show(self):
+        self.im.show()
 
-        draw.rectangle( [tl,br], fill = c, outline="black", width=2)
+    def split(self, tl, br):
+        if r.randint(0, 10) > 9:
+            self.minval = 300
+
+        if br[0] - tl[0] < self.minval or br[1] - tl[1] < self.minval:
+            # draw
+            c = self.colours[r.randint(0, len(self.colours)) - 1]
+
+            self.draw.rectangle([tl, br], fill=c, outline="black", width=2)
+
+        else:
+            p1 = (r.randint(tl[0], br[0]), r.randint(tl[1], br[1]))
+            w = r.randint(p1[0], br[0])
+            h = r.randint(p1[1], br[1])
+            p2 = (w, h)
+            a = ((tl[0], tl[1]), (p2[0], p1[1]))
+            b = ((tl[0], p1[1]), (p1[0], br[1]))
+            c = ((p2[0], tl[1]), (br[0], p2[1]))
+            d = ((p1[0], p2[1]), (br[0], br[1]))
+            e = ((p1[0], p1[1]), (p2[0], p2[1]))
+
+            self.split(a[0], a[1])
+
+            self.split(b[0], b[1])
+
+            self.split(c[0], c[1])
+
+            self.split(d[0], d[1])
+
+            self.split(e[0], e[1])
 
 
-    else:
-        p1 = (r.randint(tl[0], br[0]), r.randint(tl[1], br[1]))
-        w = r.randint(p1[0], br[0])
-        h = r.randint(p1[1], br[1])
-        p2 = (w, h)
-        '''
-        draw.line((tl[0], p1[1], p2[0], p1[1]), 0, 2)
-        draw.line((p1[0], p1[1], p1[0], br[1]), 0, 2)
-        draw.line((p1[0], p2[1], br[0], p2[1]), 0, 2)
-        draw.line((p2[0], tl[1], p2[0], p2[1]), 0, 2)
-        '''
-        a = ((tl[0], tl[1]), (p2[0], p1[1]))
-        b = ((tl[0], p1[1]), (p1[0], br[1]))
-        c = ((p2[0], tl[1]), (br[0], p2[1]))
-        d = ((p1[0], p2[1]), (br[0], br[1]))
-        e = ((p1[0], p1[1]), (p2[0], p2[1]))
 
-
-        print('running')
-        split(a[0], a[1])
-
-        split(b[0], b[1])
-
-        split(c[0], c[1])
-
-        split(d[0], d[1])
-
-        split(e[0], e[1])
-
-colours = ["#ffff33", "#ffffff", "#00f3ff", "#af00b3"]
-
-im = Image.new('RGBA', (1000, 1000), (0, 0, 0))
-draw = ImageDraw.Draw(im)
-split((0, 0), (1000, 1000))
-im.show()
+z = mondo(1000, 1000, 100)
+z.split((0, 0),(1000, 1000))
+z.show()
